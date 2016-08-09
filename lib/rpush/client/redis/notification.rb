bb83f88d7d0bf246f36b0a6789b85cefa8d96b8a
Charles Hung <chuck@lookbook.nu>
2016-08-09 15:31:37 -0700

    add dry_run parameter to notification

--- lib/rpush/client/redis/notification.rb
+++ lib/rpush/client/redis/notification.rb
@@ -54,6 +54,7 @@ module Rpush
         attribute :user_uuid, :string
         attribute :execution_key, :string
         attribute :retailer_customer_id, :string
+        attribute :dry_run, :boolean
 
 
         def app
