e788a3a9ccb08b109af5b4656cefebf80a897838
Charles Hung <chuck@lookbook.nu>
2016-11-07 13:31:48 -0800

    param to queue notifications to front of sorted set

--- lib/rpush/client/redis/notification.rb
+++ lib/rpush/client/redis/notification.rb
@@ -53,7 +53,7 @@ module Rpush
         attribute :execution_key, :string
         attribute :retailer_customer_id, :string
         attribute :dry_run, :boolean
-
+        attribute :queue_to_front, :boolean, default: false
 
         def app
           return nil unless app_id
@@ -73,7 +73,8 @@ module Rpush
 
         def register_notification
           Modis.with_connection do |redis|
-            redis.zadd(Rpush::Client::Redis::Notification.absolute_pending_namespace, id, id)
+            score = queue_to_front ? 0 : id
+            redis.zadd(Rpush::Client::Redis::Notification.absolute_pending_namespace, score, id)
           end
         end
       end
